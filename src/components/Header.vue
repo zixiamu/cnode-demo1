53084419fa060191898a0f672bd2b642764e421d
zixiamu <1214145161@qq.com>
2019-04-20 09:12:22 +0800

    "第一个vue项目"

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="header">
+        <ul>
+            <li><a href="#">首页</a></li>
+            <li><a href="#">新手入门</a></li>
+            <li><a href="#">API</a></li>
+            <li><a href="#">关于</a></li>
+            <li><a href="#">注册</a></li>
+            <li><a href="#">登陆</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Header'
+    }
+</script>
+
+
+<style scoped>
+
+</style>
